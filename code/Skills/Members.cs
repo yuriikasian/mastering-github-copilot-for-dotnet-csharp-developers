419c1640c5c0972a97b12f9a3f58e81afde40f43
yuriikasian <yuriikasian@gmail.com>
2025-08-04 17:14:08 +0300

    Add Members class to Skills namespace
    Introduced a new `Members` class with properties for
    `Name`, `Role`, and `Age`. A constructor is provided
    to initialize these properties upon instantiation.

---
+++ code/Skills/Members.cs
@@ -0,0 +1,17 @@
+﻿namespace Skills;
+
+public class Members
+{
+    public string Name { get; set; }
+
+    public string Role { get; set; }
+
+    public string Age { get; set; }
+
+    public Members(string name, string role, string age)
+    {
+        Name = name;
+        Role = role;
+        Age = age;
+    }
+}
