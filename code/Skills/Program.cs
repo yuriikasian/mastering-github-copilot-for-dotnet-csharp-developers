323c8707b13716c746909e77eb39010dc4f44c46
yuriikasian <yuriikasian@gmail.com>
2025-08-04 16:00:10 +0300

    Add basic arithmetic console app
    Introduces a new .NET 8.0 console application that reads two numbers from user input and displays their sum and difference. Includes initial Program.cs and Skills.csproj files.

---
+++ code/Skills/Program.cs
@@ -0,0 +1,18 @@
+﻿void CalculateNumbers(int var1, int var2)
+{
+    int sum = var1 + var2;
+    Console.WriteLine($"Sum: {sum}");
+}
+
+void SubstractNumbers(int var1, int var2)
+{
+    int difference = var1 - var2;
+    Console.WriteLine($"Difference: {difference}");
+}
+
+Console.WriteLine("Enter two numbers:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Results:");
+CalculateNumbers(number1, number2);
+SubstractNumbers(number1, number2);
